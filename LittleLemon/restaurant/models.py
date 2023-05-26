562335794c6a1fb019c0d2ec5720839b966081f5
JanetZhang128 <janetzhangyi@gmail.com>
2023-05-26 14:57:03 +0200

    adding apis

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -9,7 +9,7 @@ class Booking(models.Model):
     def __str__(self):
         return self.name
     
-class MenuItem(models.Model):
+class Menu(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
     inventory = models.SmallIntegerField()
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# Create MenuItemsView
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Create SingleMenuView
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Create BookingViewSet
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
